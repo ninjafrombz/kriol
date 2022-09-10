c776a5caff5c9a09f91cbcb0808561b54b737d2d
ninjafrombz <dezamagwula@gmail.com>
2022-09-09 23:57:37 -0500

    moved the code that checks for the id paremeter into it's own method readIDParam()

--- cmd/api/entries.go
+++ cmd/api/entries.go
@@ -5,9 +5,6 @@ package main
 import (
 	"fmt"
 	"net/http"
-	"strconv"
-
-	"github.com/julienschmidt/httprouter"
 )
 
 func (app *application) CreateEntryHandler(w http.ResponseWriter, r *http.Request) {
@@ -15,14 +12,12 @@ func (app *application) CreateEntryHandler(w http.ResponseWriter, r *http.Reques
 }
 
 func (app *application) showEntryHandler(w http.ResponseWriter, r *http.Request) {
-	// Use the params from context function to get the request context as a slice
-	params := httprouter.ParamsFromContext(r.Context())
-	// Get the value of the id parameter
-	id, err := strconv.ParseInt(params.ByName("id"), 10, 64)
-	if err != nil || id < 1 {
+	id, err := app.readIDParam(r)
+	if err != nil {
 		http.NotFound(w, r)
 		return
 	}
-	//Display the school id 
+
+	//Display the school id
 	fmt.Fprintf(w, "show the details for entries %d\n", id)
 }
---
+++ cmd/api/helpers.go
@@ -0,0 +1,22 @@
+// Filename: cmd/api/helpers.go
+
+package main
+
+import (
+	"errors"
+	"net/http"
+	"strconv"
+
+	"github.com/julienschmidt/httprouter"
+)
+
+func (app *application) readIDParam(r *http.Request) (int64, error) {
+	// Use the params from context function to get the request context as a slice
+	params := httprouter.ParamsFromContext(r.Context())
+	// Get the value of the id parameter
+	id, err := strconv.ParseInt(params.ByName("id"), 10, 64)
+	if err != nil || id < 1 {
+		return 0, errors.New("Invalid ID Parameter")
+	}
+	return id, nil
+}
